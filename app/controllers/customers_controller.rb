245b014e468688bc8d87e597fdc57db055d6b73a
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-08-18 11:41:35 -0500

    day2

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,7 @@
 class CustomersController < ApplicationController
+    def index
+        @customers=Customer.all 
+    end
     def show
         @customer=Customer.find(params[:id])
     end
@@ -9,9 +12,23 @@ class CustomersController < ApplicationController
         @customer=Customer.create(customer_params)
         redirect_to customer_path(@customer)
     end
-    
+    def edit
+        @customer=Customer.find(params[:id])
+    end
+    def update
+        @customer=Customer.find(params[:id])
+        @customer.update(customer_params)
+        redirect_to customer_path(@customer)
+    end
+
+    def destroy
+        @customer=Customer.find(params[:id])
+        @customer.destroy
+        redirect_to customers_path
+    end
+
     private
     def customer_params
-        params.require(:customer).permit(:name,:age, :phone_number,:address,:email,:salary, card_ids:[],cards_attributes:[:name])
+        params.require(:customer).permit(:name,:age, :phone_number,:address,:email,:salary, card_ids:[])
     end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,10 +3,4 @@ class Customer < ApplicationRecord
     has_many :cards, through: :customer_cards
     has_many :transactions
 
-    def cards_attributes=(card_attributes)
-        cards_attributes.values.each do |card_attributes|
-            card=Card.find_or_create_by(name:card_attributes[:name])
-            self.card<<card
-        end
-    end
 end
