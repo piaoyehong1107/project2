f450bd29181ecd6fde78ff550c161d307c6161ff
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-08-18 15:23:13 -0500

    day2update

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,26 @@
 class TransactionsController < ApplicationController
-   
+   def index
+    @transactions=Transaction.all
+   end
+
+   def show
+    @transaction=Transaction.find(params[:id])
+
+   end
+
+   def new 
+    @transaction=Transaction.new
+   end
+
+   def create
     
+    @transaction=Transaction.create(transaction_params)
+    redirect_to transaction_path(@transaction)
+   end
+
+   private
+   def transaction_params
+    params.require(:transaction).permit(:amount, :card_id, :customer_id)
+   end
 
 end
