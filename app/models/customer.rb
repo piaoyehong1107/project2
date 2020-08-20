3563281ef9327cb2b9ae1341a613725d177c60b3
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-08-20 10:46:17 -0500

    thu1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,9 @@ class Customer < ApplicationRecord
     has_many :customer_cards
     has_many :cards, through: :customer_cards
     has_many :transactions, dependent: :delete_all
+
+    validates :name, presence: true
+    validates :age, numericality: { greater_than: 19}
+    validates :salary, numericality: { other_than: 0 }
+    
 end
