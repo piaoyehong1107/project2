e0ab163beb42810805ad3a19ea04d0dcf35443b0
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-08-20 11:00:38 -0500

    thu2

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ApplicationRecord
 
     validates :name, presence: true
     validates :age, numericality: { greater_than: 19}
+    validates :eamil, uniqueness: true
     validates :salary, numericality: { other_than: 0 }
-    
+
 end
