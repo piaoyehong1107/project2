3ff95a833e36999db336e3c19fe82087f1ebdcf3
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-08-19 22:31:19 -0500

    wed3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :transactions
   resources :customer_cards
-  resources :customers
+  resources :customers do 
+    resources :transactions
+  end
   resources :cards
   resources :banks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
